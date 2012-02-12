3c25c368b46005008b5d5ec48f4d041609bb5eac
Jacob Norris <jacobnorris44@gmail.com>
2012-02-12 13:30:23 -0800

    Very basic skeleton app added:    - App uses buttons to switch between activities, and go back      to the "homepage". Different activities are denoted by the header at      the top of the screen.

--- CHIPSApp/src/com/Chips/CHIPSAppActivity.java
+++ CHIPSApp/src/com/Chips/CHIPSAppActivity.java
@@ -1,7 +1,15 @@
 package com.Chips;
 
+//import com.example.HelloFormStuff.HelloFormStuffActivity;
+//import com.example.HelloFormStuff.R;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CHIPSAppActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +17,75 @@ public class CHIPSAppActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // For the Shopping Lists button.
+    final Button button = (Button) findViewById(R.id.shopping_lists);
+        button.setOnClickListener(new OnClickListener() {
+        public void onClick(View v) {
+            // Perform action on clicks
+        	
+        	Toast.makeText(CHIPSAppActivity.this, "Going to shopping lists...", Toast.LENGTH_SHORT).show();
+        	
+        	Intent intent = new Intent(CHIPSAppActivity.this, ShoppingLists.class);
+        	startActivity(intent);
+        }
+        }); 
+        // -----------------------
+        
+     // For the Calendar button.
+        final Button button2 = (Button) findViewById(R.id.calendar);
+            button2.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on clicks
+            	
+            	Toast.makeText(CHIPSAppActivity.this, "Going to calendar...", Toast.LENGTH_SHORT).show();
+            	
+            	Intent intent = new Intent(CHIPSAppActivity.this, Calendar.class);
+            	startActivity(intent);
+            }
+            }); 
+            // -----------------------
+            
+         // For the Inventory button.
+            final Button button3 = (Button) findViewById(R.id.inventory);
+                button3.setOnClickListener(new OnClickListener() {
+                public void onClick(View v) {
+                    // Perform action on clicks
+                	
+                	Toast.makeText(CHIPSAppActivity.this, "Going to inventory...", Toast.LENGTH_SHORT).show();
+                	
+                	Intent intent = new Intent(CHIPSAppActivity.this, Inventory.class);
+                	startActivity(intent);
+                }
+                }); 
+                // -----------------------
+            
+             // For the Preferences button.
+                final Button button4 = (Button) findViewById(R.id.preferences);
+                    button4.setOnClickListener(new OnClickListener() {
+                    public void onClick(View v) {
+                        // Perform action on clicks
+                    	
+                    	Toast.makeText(CHIPSAppActivity.this, "Going to preferences...", Toast.LENGTH_SHORT).show();
+                    	
+                    	Intent intent = new Intent(CHIPSAppActivity.this, Preferences.class);
+                    	startActivity(intent);
+                    }
+                    }); 
+                    // -----------------------
+                    
+                 // For the Statistics button.
+                    final Button button5 = (Button) findViewById(R.id.statistics);
+                        button5.setOnClickListener(new OnClickListener() {
+                        public void onClick(View v) {
+                            // Perform action on clicks
+                        	
+                        	Toast.makeText(CHIPSAppActivity.this, "Going to statistics...", Toast.LENGTH_SHORT).show();
+                        	
+                        	Intent intent = new Intent(CHIPSAppActivity.this, Statistics.class);
+                        	startActivity(intent);
+                        }
+                        }); 
+                        // -----------------------
     }
 }
